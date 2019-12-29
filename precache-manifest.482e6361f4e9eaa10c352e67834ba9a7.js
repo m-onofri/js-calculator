280166c7b12de015af171597dbee838fcad5358b
m-onofri <manuele.onofri@gmail.com>
2019-12-29 17:33:24 +0100

    Updates

---
+++ precache-manifest.482e6361f4e9eaa10c352e67834ba9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccb5fe330735dcb05427348bec3d515",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "84fc136359028e99f66f",
+    "url": "/js-calculator/static/css/main.5a18a4bf.chunk.css"
+  },
+  {
+    "revision": "1c53b91e5591e42722e7",
+    "url": "/js-calculator/static/js/2.6d3e0135.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/js-calculator/static/js/2.6d3e0135.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84fc136359028e99f66f",
+    "url": "/js-calculator/static/js/main.7a8ae42e.chunk.js"
+  },
+  {
+    "revision": "55fa15c6b8a8379aa2be",
+    "url": "/js-calculator/static/js/runtime-main.de46c35a.js"
+  }
+]);
